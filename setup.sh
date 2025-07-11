8f9145f09c5d00c1ce4a0fd75418cc65fd2d1329
Nexus Embedded <nexus@embedded.com>
2025-07-11 15:03:58 +0000

    fixed path CONCERT_LAUNCHER

--- setup.sh
+++ setup.sh
@@ -4,4 +4,4 @@ export XBOT2_DEFAULT_HW=ec_idle
 export ECAT_MASTER_CONFIG="$SCRIPT_DIR/ecat/ecat_config.yaml"
 alias ecat_master="stdbuf --output=L --error=L repl $ECAT_MASTER_CONFIG 2>&1 | tee /tmp/ecat-output"
 alias ecat_master_gdb="gdb --args repl $ECAT_MASTER_CONFIG"
-export CONCERT_LAUNCHER_DEFAULT_CONFIG="$SCRIPT_DIR/gui/nexus_launcher_config.yaml".
+export CONCERT_LAUNCHER_DEFAULT_CONFIG="$SCRIPT_DIR/gui/nexus_launcher_config.yaml"
