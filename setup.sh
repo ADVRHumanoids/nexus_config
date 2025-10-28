045114c1427b50a3ecf532ffad0192b094ee3d75
Nexus Embedded <nexus@embedded.com>
2025-10-28 14:04:49 +0000

    ros2 implementation

--- setup.sh
+++ setup.sh
@@ -4,5 +4,5 @@ export XBOT2_DEFAULT_HW=ec_idle
 export ECAT_MASTER_CONFIG="$SCRIPT_DIR/ecat/ecat_config.yaml"
 alias ecat_master="stdbuf --output=L --error=L repl -f $ECAT_MASTER_CONFIG 2>&1 | tee /tmp/ecat-output"
 alias ecat_master_gdb="gdb --args repl $ECAT_MASTER_CONFIG"
-export CONCERT_LAUNCHER_DEFAULT_CONFIG="$SCRIPT_DIR/gui/ros1/nexus_launcher_config.yaml"
+export CONCERT_LAUNCHER_DEFAULT_CONFIG="$SCRIPT_DIR/gui/ros2/nexus_launcher_config.yaml"
 set_xbot2_config "$SCRIPT_DIR/nexus_basic.yaml"
